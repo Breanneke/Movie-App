1e3c85aa7ec2b8d1468f13412ccc9f92d29b66b5
Brian Ochieng <ochiengbrian634@gmail.com>
2024-03-24 13:41:01 +0300

    modified:   css/style.css modified:   index.html modified:   js/script.js new file:   play-page.html

--- js/script.js
+++ js/script.js
@@ -36,3 +36,12 @@ var swiper = new Swiper(".popular-content", {
     },
   },
 });
+
+// show videos
+let playButton = document.querySelector(".play-movie");
+let video = document.querySelector(".video-container");
+let closebtn = document.querySelector(".close-video");
+
+playButton.onclick = () => {
+  video.classList.add("show-video");
+};
